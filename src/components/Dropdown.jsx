a364ffd069cfb62ed50c8faf3cbe77193d271804
DingoDev <randilea@outlook.com>
2026-01-22 15:06:27 -0700

    Creating dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,27 +3,6 @@ import React, { useState } from "react";
 // import AuthorData from "./AuthorData";
 
 function AuthorDropdown({}) {
-  //   const [selected, setSelected] = useState(null);
-  //   const handleSelect = (eventKey) => {
-  //     setSelected(eventKey);
-  //     onChange?.(eventKey);
-  //   };
-  //   return (
-  //     <div className="dropdown">
-  //       <Dropdown onSelect={handleSelect}>
-  //         <Dropdown.Toggle variant="light" id="dropdown-basic">
-  //           {selected || "Select an Author:"}
-  //         </Dropdown.Toggle>
-  //         <Dropdown.Menu>
-  //           {AuthorData.map((author) => (
-  //             <Dropdown.Item key={author.authorId} eventKey={author.authorName}>
-  //               {author.authorName}
-  //             </Dropdown.Item>
-  //           ))}
-  //         </Dropdown.Menu>
-  //       </Dropdown>
-  //     </div>
-  //   );
   return (
     <Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
