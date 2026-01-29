27777bbfdd0b773c8d77d39c76100ac2508de935
DingoDev <randilea@outlook.com>
2026-01-29 15:38:58 -0700

    Styled button

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -5,14 +5,14 @@ import React, { useState } from "react";
 function AuthorDropdown({}) {
   return (
     <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
+      <Dropdown.Toggle variant="clear" id="dropdown-basic">
+        Select an author:
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
         <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
       </Dropdown.Menu>
     </Dropdown>
   );
